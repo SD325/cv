ee46868c781931b632f099f4fb9f9e31edc10ac3
Spandan Das <spandand515@gmail.com>
2019-10-20 12:23:45 -0400

    not working p3 (again) --> trying new version

--- 02_closest_pair_p3.cpp
+++ 02_closest_pair_p3.cpp
@@ -9,10 +9,9 @@
 #include <vector>
 
 #define N 200 // resolution of ppm file
-#define num_pts 5 // number of points
+#define num_pts 20 // number of points
 
 using namespace std;
-
 
 static bool y_sorting = false;
 class Point {
@@ -72,67 +71,42 @@ void drawpt(int x, int y, int r, int g, int b) {
     ppm[x][y].b = b;
 }
 
-vector<int> brute_force(Point p[], int len) {
+vector<int> brute_force(vector<int> curr, int len) {
     double min_dist = LONG_MAX;
-    double old;
+    double this_dist = 0;
     vector<int> minInd(2);
     for (int i = 0; i < len; i++) {
         for (int j = i+1; j < len; j++) {
-            old = min_dist;
-            min_dist = min(min_dist, dist(p[i], p[j]));
-            if (min_dist < old) {
-                minInd.at(0) = i;
-                minInd.at(1) = j;
+            this_dist = dist(pts[curr.at(i)], pts[curr.at(j)]);
+            if (this_dist < min_dist) {
+                minInd.at(0) = curr.at(i);
+                minInd.at(1) = curr.at(j);
+                min_dist = this_dist;
             }
         }
     }
     return minInd;
 }
 
-vector<int> closest_in_strip(Point strip[], int len, double min_dist, vector<int> currMin) {
-    // *******************************
-    // NEED TO CHANGE!!!!!!!!!!!!!!!!!
 
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len && (strip[j].y - strip[i].y) < min_dist; ++j) {
-            if (dist(strip[i], strip[j]) < min_dist) {
-                min_dist = dist(strip[i], strip[j]);
-                currMin.at(0) = i;
-                currMin.at(1) = j;
-            }
-        }
-    }
-    return currMin;
-}
-
-
-vector<int> closest_helper(Point sortedX[], Point sortedY[], int len) {
+vector<int> merge_helper(vector<int> curr, int len, Point y_sorted[]) {
     if (len <= 3) {
-        return brute_force(sortedX, len);
+        return brute_force(curr, len);
     }
     else {
         int mid = len/2;
-        Point midX = sortedX[mid];
-        Point left_sortedY[mid+1];
-        int leftInd = 0;
-        Point right_sortedY[len - (mid+1)];
-        int rightInd = 0;
-        for (int i = 0; i < len; i++) {
-            if (sortedY[i].x <= midX.x) {
-                left_sortedY[leftInd] = sortedY[i];
-                leftInd++;
-            }
-            else {
-                right_sortedY[rightInd] = sortedY[i];
-                rightInd++;
-            }
+        vector<int> left;
+        vector<int> right;
+        for (int i = 0; i < mid; i++) {
+            left.push_back(curr.at(i));
         }
-
-
-        vector<int> left_min = closest_helper(sortedX, left_sortedY, mid);
-        vector<int> right_min = closest_helper(sortedX + mid, right_sortedY, len - mid);
-        double left_dist = dist(sortedX[left_min.at(0)], sortedX[left_min.at(1)]);
-        double right_dist = dist(sortedX[right_min.at(0) + mid], sortedX[right_min.at(1) + mid]);
+        for (int i = mid; i < len; i++) {
+            right.push_back(curr.at(i));
+        }
+        vector<int> left_min = merge_helper(left, left.size());
+        vector<int> right_min = merge_helper(right, right.size());
+        double left_dist = dist(pts[left_min.at(0)], pts[left_min.at(1)]);
+        double right_dist = dist(pts[right_min.at(0)], pts[right_min.at(1)]);
         double min_dist;
         vector<int> min_ind;
         if (left_dist < right_dist) {
@@ -142,48 +116,45 @@ vector<int> closest_helper(Point sortedX[], Point sortedY[], int len) {
         }
         else {
             min_dist = right_dist;
-            min_ind.push_back(right_min.at(0)+mid);
-            min_ind.push_back(right_min.at(1)+mid);
+            min_ind.push_back(right_min.at(0));
+            min_ind.push_back(right_min.at(1));
         }
 
-        //double mid_x = (pts[curr.at(mid)].x + pts[curr.at(mid+1)].x) / 2.0;
-        //double mid_x = pts[curr.at(mid)].x;
-        Point inside_strip[len];
-        int strip_ind = 0;
+        double mid_x = pts[curr.at(mid)].x;
+        vector<int> inside_strip;
         for (int i = 0; i < len; i++) {
-            if (abs(sortedX[i].x - midX.x) < min_dist) {
-                inside_strip[strip_ind] = sortedX[i];
-                strip_ind++;
+            if (abs(pts[curr.at(i)].x - mid_x) < min_dist) {
+                inside_strip.push_back(curr.at(i));
             }
         }
-        return closest_in_strip(inside_strip, strip_ind, min_dist, min_ind);
+
+
+        // minimum inside strip
 //        double this_dist = 0;
-//        for (int i = 0; i < len; i++) {
-//            for (int j = i+1; j < len; j++) {
-//                this_dist = dist(pts[curr.at(i)], pts[curr.at(j)]);
+//        for (int i = 0; i < (int) inside_strip.size(); i++) {
+//            for (int j = i+1; j < (int) inside_strip.size(); j++) {
+//                this_dist = dist(pts[inside_strip.at(i)], pts[inside_strip.at(j)]);
 //                if (this_dist < min_dist) {
 //                    min_dist = this_dist;
-//                    min_ind.at(0) = curr.at(i);
-//                    min_ind.at(1) = curr.at(j);
+//                    min_ind.at(0) = inside_strip.at(i);
+//                    min_ind.at(1) = inside_strip.at(j);
 //                }
 //            }
 //        }
-//        return min_ind;
+        return min_ind;
     }
 }
 
 
 vector<int> closest() {
-    Point x_sorted[num_pts];
     Point y_sorted[num_pts];
-    vector<int> temp;
-    for (int i = 0; i < num_pts; i++) {
-        x_sorted[i] = pts[i];
-        y_sorted[i] = pts[i];
-    }
-    sort(x_sorted, x_sorted+num_pts);
+    for (int i = 0; i < num_pts; i++) y_sorted[i] = pts[i];
+    sort(pts, pts+num_pts);
+    y_sorting = true;
     sort(y_sorted, y_sorted+num_pts);
-    return closest_helper(x_sorted, y_sorted, num_pts);
+    vector<int> temp;
+    for (int i = 0; i < num_pts; i++) temp.push_back(i);
+    return merge_helper(temp, num_pts, y_sorted);
 }
 
 
