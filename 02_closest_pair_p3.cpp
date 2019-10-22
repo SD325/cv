a2b20a6577e6999e56920e3226f21fc10c7d8d49
Spandan Das <spandand515@gmail.com>
2019-10-22 15:35:28 -0400

    finalizing

--- 02_closest_pair_p3.cpp
+++ 02_closest_pair_p3.cpp
@@ -9,8 +9,8 @@
 #include <vector>
 #include <chrono>
 
-#define N 200 // resolution of ppm file
-#define num_pts 10 // number of points
+#define N 800 // resolution of ppm file
+#define num_pts 500000 // number of points
 
 using namespace std;
 
@@ -63,7 +63,7 @@ double random() {
 }
 
 double dist(Point p1, Point p2) {
-    return sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+    return ((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
 }
 
 void drawpt(int x, int y, int r, int g, int b) {
@@ -194,15 +194,65 @@ vector<int> closest_helper(vector<int> x_sorted, vector<int> y_sorted, int len) 
 }
 
 
-vector<int> closest_find() {
+vector<int> merge_helper(vector<int> curr, int len) {
+    if (len <= 3) {
+        return brute_force(curr, len);
+    }
+    else {
+        int mid = len/2;
+        vector<int> left;
+        vector<int> right;
+        for (int i = 0; i < mid; i++) left.push_back(curr.at(i));
+        for (int i = mid; i < len; i++) right.push_back(curr.at(i));
+        vector<int> left_min = merge_helper(left, left.size());
+        vector<int> right_min = merge_helper(right, right.size());
+        double left_dist = dist(pts[left_min.at(0)], pts[left_min.at(1)]);
+        double right_dist = dist(pts[right_min.at(0)], pts[right_min.at(1)]);
+        double min_dist;
+        vector<int> min_ind;
+        if (left_dist < right_dist) {
+            min_dist = left_dist;
+            min_ind.push_back(left_min.at(0));
+            min_ind.push_back(left_min.at(1));
+        }
+        else {
+            min_dist = right_dist;
+            min_ind.push_back(right_min.at(0));
+            min_ind.push_back(right_min.at(1));
+        }
+
+        double mid_x = (pts[curr.at(mid)].x + pts[curr.at(mid+1)].x) / 2.0;
+        //double mid_x = pts[curr.at(mid)].x;
+        vector<int> inside_strip;
+        for (int i = 0; i < len; i++) {
+            if (abs(pts[curr.at(i)].x - mid_x) < min_dist) {
+                inside_strip.push_back(curr.at(i));
+            }
+        }
+
+        double this_dist = 0;
+        for (int i = 0; i < (int) inside_strip.size(); i++) {
+            for (int j = i+1; j < (int) inside_strip.size(); j++) {
+                this_dist = dist(pts[inside_strip.at(i)], pts[inside_strip.at(j)]);
+                if (this_dist < min_dist) {
+                    min_dist = this_dist;
+                    min_ind.at(0) = inside_strip.at(i);
+                    min_ind.at(1) = inside_strip.at(j);
+                }
+            }
+        }
+        return min_ind;
+    }
+}
+
+
+vector<int> closest_find(vector<int> temp) {
     for (int i = 0; i < num_pts; i++) {
         y_order[i] = pts[i];
     }
     sort(pts, pts+num_pts);
     y_sorting = true;
     sort(y_order, y_order + num_pts);
-    vector<int> temp;
-    for (int i = 0; i < num_pts; i++) temp.push_back(i);
     return closest_helper(temp, temp, num_pts);
 }
 
@@ -212,24 +262,26 @@ int main() {
     vector<int> temp;
     for (int i = 0; i < num_pts; i++) temp.push_back(i);
     // white background
-    for (auto &i : ppm) {
-        for (auto &j : i) {
-            j.r = 1;
-            j.g = 1;
-            j.b = 1;
-        }
-    }
+//    for (auto &i : ppm) {
+//        for (auto &j : i) {
+//            j.r = 1;
+//            j.g = 1;
+//            j.b = 1;
+//        }
+//    }
     auto start = chrono::high_resolution_clock::now();
     for (int trials = 0; trials < 1; trials++) {
         for (auto &pt : pts) {
             pt = Point(random(), random());
-            int roundedX = (int) (N * pt.x);
-            int roundedY = (int) (N * pt.y);
-            drawpt(roundedX, roundedY, 0, 0, 0);
+//            int roundedX = (int) (N * pt.x);
+//            int roundedY = (int) (N * pt.y);
+//            drawpt(roundedX, roundedY, 0, 0, 0);
         }
 
-        vector<int> brute = brute_force(temp, num_pts);
-        vector<int> minInd = closest_find();
+        vector<int> minInd = brute_force(temp, num_pts);
+        //vector<int> minInd = closest_find(temp);
+        //vector<int> minInd = merge_helper(temp, num_pts);
+
 //        cout << pts[brute.at(0)].x << " " << pts[brute.at(0)].y << endl;
 //        cout << pts[brute.at(1)].x << " " << pts[brute.at(1)].y << endl;
 //        drawpt((int) (N * pts[brute.at(0)].x), (int) (N * pts[brute.at(0)].y), 0, 1, 0);
@@ -249,15 +301,15 @@ int main() {
 
 
 //    // WRITE TO PPM
-    ofstream image("02_closest_pair.ppm");
-    image << "P3 " << N << " " << N << " 1" << endl;
-
-    for (auto &i : ppm) {
-        for (auto &j : i) {
-            image << j.r << " " << j.g << " " << j.b << " ";
-        }
-        image << endl;
-    }
-    image.close();
+//    ofstream image("02_closest_pair.ppm");
+//    image << "P3 " << N << " " << N << " 1" << endl;
+//
+//    for (auto &i : ppm) {
+//        for (auto &j : i) {
+//            image << j.r << " " << j.g << " " << j.b << " ";
+//        }
+//        image << endl;
+//    }
+//    image.close();
     return 0;
 }
