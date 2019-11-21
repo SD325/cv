ef07d3227835d147dffa2f43ccb9a5e4831538da
Spandan Das <spandand515@gmail.com>
2019-11-21 13:38:34 -0500

    finished

--- 02_closest_pair_p5.cpp
+++ 02_closest_pair_p5.cpp
@@ -1,6 +1,5 @@
-//
-// Created by Spandan Das on 9/9/2019.
-//
+// Spandan Das
+// Period 4
 
 
 #include <iostream>
@@ -8,16 +7,13 @@
 #include <ctime>
 #include <fstream>
 #include <algorithm>
+#include <utility>
 #include <vector>
 #include <chrono>
 #include <unordered_map>
 
-#define num_pts 10000 // number of points
-#define num_trials 1
-
 using namespace std;
 
-bool y_sorting = false;
 class Point {
 public:
     double x;
@@ -32,16 +28,14 @@ public:
         Point(0, 0);
     }
     bool operator <(const Point & pt) const {
-        if (y_sorting) return y < pt.y;
         return x < pt.x;
     }
 
 };
 
 
-// coordinates of vertices are doubles
-static Point pts[num_pts];
-static Point y_order[num_pts];
+static vector<Point> pts;
+static int num_pts;
 
 double random_double() {
     return (double) rand() / RAND_MAX;
@@ -50,25 +44,6 @@ double random_double() {
 double dist(Point p1, Point p2) {
     return sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
 }
-
-
-vector<int> brute_force(vector<int> x_sorted, int len) {
-    double min_dist = 2;
-    double this_dist = 0;
-    vector<int> minInd(2);
-    for (int i = 0; i < len; i++) {
-        for (int j = i+1; j < len; j++) {
-            this_dist = dist(pts[x_sorted.at(i)], pts[x_sorted.at(j)]);
-            if (this_dist < min_dist) {
-                minInd.at(0) = x_sorted.at(i);
-                minInd.at(1) = x_sorted.at(j);
-                min_dist = this_dist;
-            }
-        }
-    }
-    return minInd;
-}
-
 
 
 void make_dictionary(int num_squares, double side, unordered_map<int, int> *dict, int index) {
@@ -124,143 +99,27 @@ vector<int> rand_finder() {
 }
 
 
-vector<int> closest_helper(vector<int> x_sorted, vector<int> y_sorted, int len) {
-    if (len <= 3) {
-        return brute_force(x_sorted, len);
-    }
-    else {
-        int mid = len/2;
-        Point mid_x = pts[x_sorted.at(mid)];
-        vector<int> x_left;
-        vector<int> y_left;
-        vector<int> x_right;
-        vector<int> y_right;
-        for (int i = 0; i < mid; i++) {
-            x_left.push_back(x_sorted.at(i));
-            if (i >= y_sorted.size()) continue;
-            if (y_order[y_sorted.at(i)].x < mid_x.x) y_left.push_back(y_sorted.at(i));
-            else y_right.push_back(y_sorted.at(i));
-        }
-        for (int i = mid; i < len; i++) {
-            x_right.push_back(x_sorted.at(i));
-            if (i >= y_sorted.size()) continue;
-            if (y_order[y_sorted.at(i)].x < mid_x.x) {
-                y_left.push_back(y_sorted.at(i));
-            }
-            else {
-                y_right.push_back(y_sorted.at(i));
-            }
-        }
-
-        vector<int> left_min = closest_helper(x_left, y_left, x_left.size());
-        vector<int> right_min = closest_helper(x_right, y_right, x_right.size());
-        double left_dist = dist(pts[left_min.at(0)], pts[left_min.at(1)]);
-        double right_dist = dist(pts[right_min.at(0)], pts[right_min.at(1)]);
-        double min_dist;
-        vector<int> min_ind;
-
-        // if the points are the same, then don't include
-        if (left_dist == 0) {
-            min_dist = right_dist;
-            min_ind.push_back(right_min.at(0));
-            min_ind.push_back(right_min.at(1));
-        }
-        else if (right_dist == 0) {
-            min_dist = left_dist;
-            min_ind.push_back(left_min.at(0));
-            min_ind.push_back(left_min.at(1));
-        }
-        else if (left_dist < right_dist) {
-            min_dist = left_dist;
-            min_ind.push_back(left_min.at(0));
-            min_ind.push_back(left_min.at(1));
-        }
-        else {
-            min_dist = right_dist;
-            min_ind.push_back(right_min.at(0));
-            min_ind.push_back(right_min.at(1));
-        }
-
-        vector<int> inside_strip;
-        for (int i = 0; i < len; i++) {
-            if (i >= y_sorted.size()) break;
-            if (abs(y_order[y_sorted.at(i)].x - mid_x.x) < min_dist) {
-                inside_strip.push_back(y_sorted.at(i));
-            }
-        }
-
-        vector<int> min_ind_in_strip;
-        min_ind_in_strip.push_back(-1);
-        min_ind_in_strip.push_back(-2); // temp values
-        double min_dist_in_strip = 2;
-        double this_dist = 0;
-        for (int i = 0; i < (int) inside_strip.size(); i++) {
-            for (int j = i+1; j < (int) inside_strip.size() && j < i+8; j++) {
-                this_dist = dist(y_order[inside_strip.at(i)], y_order[inside_strip.at(j)]);
-                if (this_dist < min_dist_in_strip) {
-                    min_dist_in_strip = this_dist;
-                    min_ind_in_strip.at(0) = inside_strip.at(i);
-                    min_ind_in_strip.at(1) = inside_strip.at(j);
-                }
-            }
-        }
-
-        if (min_dist_in_strip < min_dist) {
-            // convert y_sorted index to pts index
-            Point py0 = y_order[min_ind_in_strip.at(0)];
-            Point py1 = y_order[min_ind_in_strip.at(1)];
-            for (int i = 0; i < len; i++) {
-                if (py0.x == pts[x_sorted.at(i)].x && py0.y == pts[x_sorted.at(i)].y) {
-                    min_ind.at(0) = x_sorted.at(i);
-                    break;
-                }
-            }
-            for (int i = 0; i < len; i++) {
-                if (py1.x == pts[x_sorted.at(i)].x && py1.y == pts[x_sorted.at(i)].y) {
-                    min_ind.at(1) = x_sorted.at(i);
-                    break;
-                }
-            }
-        }
-        return min_ind;
-    }
+void findClosestPair(vector<Point> points) {
+    pts = std::move(points);
+    random_shuffle(pts.begin(), pts.end());
+    vector<int> randomized_results = rand_finder();
+    cout << pts[randomized_results[0]].x << " " << pts[randomized_results[0]].y << endl;
+    cout << pts[randomized_results[1]].x << " " << pts[randomized_results[1]].y << endl;
 }
-
-
-vector<int> closest_find(const vector<int>& temp) {
-    for (int i = 0; i < num_pts; i++) {
-        y_order[i] = pts[i];
-    }
-    sort(pts, pts+num_pts);
-    y_sorting = true;
-    sort(y_order, y_order + num_pts);
-    return closest_helper(temp, temp, num_pts);
-}
-
-
 
 int main() {
     srand(time(nullptr));
-    vector<int> temp;
-    for (int i = 0; i < num_pts; i++) temp.push_back(i);
 
-    for (auto &pt : pts) {
-        pt = Point(random_double(), random_double());
+    ifstream infile("points.txt");
+    infile >> num_pts;
+    vector<Point> points;
+    double x, y;
+    while (infile) {
+        infile >> x >> y;
+        points.emplace_back(x, y);
     }
-
-    auto startRec = chrono::high_resolution_clock::now();
-    vector<int> recursive_results = closest_find(temp);
-    auto stopRec = chrono::high_resolution_clock::now();
-    auto durationRec = chrono::duration_cast<chrono::microseconds>(stopRec - startRec);
-    cout << "Recursive Time: " << durationRec.count()/1e6 << endl << "-----" << endl;
-
-
-    auto startRand = chrono::high_resolution_clock::now();
-    random_shuffle(pts, pts+num_pts);
-    vector<int> randomized_results = rand_finder();
-    auto stopRand = chrono::high_resolution_clock::now();
-    auto durationRand = chrono::duration_cast<chrono::microseconds>(stopRand - startRand);
-    cout << "Randomized Time: " << durationRand.count()/1e6 << endl << "-----" << endl;
+    infile.close();
+    findClosestPair(points);
 
     return 0;
 }
